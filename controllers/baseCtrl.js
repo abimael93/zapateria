ebad4db8b70821a2c39bfe71e7f0d3ea8c802227
abimael93 <chris.abimael93@gmail.com>
2014-12-24 15:32:30 -0600

    Rutas conectadas con sus respectivas vistas, modificacion del Index y Controlador Base
    Falta maquetar las vistas y agregar mas módulos

--- controllers/baseCtrl.js
+++ controllers/baseCtrl.js
@@ -2,29 +2,69 @@ angular.module('Rutas', ['ngRoute'])
 
 .config(function($routeProvider){
 	$routeProvider
+		//Inicio
 		.when('/', {
 			templateUrl: 'views/home.html'
 		})
-		.when('/empleado/alta', {
+		//Módulo Empleado
+		.when('/empleado/create', {
 			templateUrl: 'views/empleado_form.html'
 		})
-		.when('/empleado/listar', {
-			//controller: 'ControladorEmpleados',
+		.when('/empleado/list', {
 			templateUrl: 'views/empleado_list.html'
 		})
-		.when('/cliente/alta', {
+		//Módulo Cliente
+		.when('/cliente/create', {
 			templateUrl: 'views/cliente_form.html'
 		})
-		.when('/cliente/listar', {
-			//controller: 'Controladorclientes',
+		.when('/cliente/list', {
 			templateUrl: 'views/cliente_list.html'
 		})
-		.when('/proveedor/alta', {
+		//Módulo Proveedor
+		.when('/proveedor/create', {
 			templateUrl: 'views/proveedor_form.html'
 		})
-		.when('/proveedor/listar', {
-			//controller: 'Controladorproveedors',
+		.when('/proveedor/list', {
 			templateUrl: 'views/proveedor_list.html'
+		})
+		//Módulo Almacen
+		.when('/almacen/list', {
+			templateUrl: 'views/almacen_list.html'
+		})
+		//Módulo Producto
+		.when('/producto/create', {
+			templateUrl: 'views/producto_form.html'
+		})
+		.when('/producto/list', {
+			templateUrl: 'views/producto_list.html'
+		})
+		//Módulo Remisión
+		.when('/remision/create', {
+			templateUrl: 'views/remision_form.html'
+		})
+		.when('/remision/list', {
+			templateUrl: 'views/remision_list.html'
+		})
+		//Módulo Recepción
+		.when('/recepcion/create', {
+			templateUrl: 'views/recepcion_form.html'
+		})
+		.when('/recepcion/list', {
+			templateUrl: 'views/recepcion_list.html'
+		})
+		//Módulo Ajuste Entrada
+		.when('/ajuste_entrada/create', {
+			templateUrl: 'views/ajuste_entrada_form.html'
+		})
+		.when('/ajuste_entrada/list', {
+			templateUrl: 'views/ajuste_entrada_list.html'
+		})
+		//Módulo Ajuste Salida
+		.when('/ajuste_salida/create', {
+			templateUrl: 'views/ajuste_salida_form.html'
+		})
+		.when('/ajuste_salida/list', {
+			templateUrl: 'views/ajuste_salida_list.html'
 		})
 		.otherwise({
 			redirectTo: '/'
