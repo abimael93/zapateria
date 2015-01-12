6a914d36bffe5b5f623cc9652d670bed2b850439
rmn528 <gerardo528-1@hotmail.com>
2015-01-11 20:09:02 -0600

    problema de merge

---
+++ app/models/Nota.php
@@ -0,0 +1,18 @@
+<?php
+
+class Nota extends Eloquent {
+    use Utiles;
+    protected $table                = 'nota';
+    protected $primaryKey           = 'id_nota';
+    public    $timestamps           = false;
+    protected $fillable             = array(
+                                                'id_objeto',
+                                                'tipo_objeto',
+                                                'id_autor',
+                                                'comentario',
+                                                'fecha_registro',
+                                            );
+    protected static $relaciones    = array(
+                                            );
+}
+?>
