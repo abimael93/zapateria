d96ad437119d20e2952a50750639a88bed598e48
cesar <kyele936@gmail.com>
2015-01-12 00:48:46 -0600

    Agrege Modulo de Tarea y Produccion

---
+++ app/models/Produccion.php
@@ -0,0 +1,37 @@
+<?php
+
+class Produccion extends Eloquent {
+    use Utiles;
+    
+    protected $table = 'cliente';
+
+    protected $primaryKey           = 'id_produccion';
+    public    $timestamps           = false;
+    protected $fillable             = array(
+                                               'id_pedido_detalle',
+                                               'id_supervisor',
+                                               'folio',
+                                               'cantidad',
+                                               'fecha_registro',
+                                               'estatus',
+                                               'eliminado',
+                                           );
+    protected static $relaciones    = array(
+                                                'empleado',
+                                                'pedido',
+                                                'tarea',
+                                            );
+
+    public function empleado () {
+        return $this->belongsTo('Empleado','id_supervisor');
+    }
+
+    public function tarea () {
+        return $this->hasMany('Tarea','id_produccion');
+    }
+    
+    public function Pedido () {
+        return $this->belongsToMany('Pedido','pedido_detalle','id_produccion','id_pedido')->withPivot('cantidad', 'precio_unitario','descuento');     
+    }
+}
+?>
---
+++ app/models/tarea.php
@@ -0,0 +1,48 @@
+<?php
+
+class Tarea extends Eloquent {
+    use Utiles;
+    
+    protected $table = 'cliente';
+
+    protected $primaryKey           = 'id_tarea';
+    public    $timestamps           = false;
+    protected $fillable             = array(
+                                               'id_tarea_padre',
+                                               'id_empleado',
+                                               'id_proceso',
+                                               'id_produccion',
+                                               'estatus',
+                                               'estatus',
+                                               'eliminado',
+                                               'fecha_registro',              
+                                           );
+    protected static $relaciones    = array(
+                                                'tarea_padre'
+                                                'tarea'
+                                                'proceso',
+                                                'empleado',
+                                                'produccion',
+                                            );
+
+    public function tarea_padre () {
+        return $this->hasMany('Tarea','id_tarea_padre');
+    }
+
+    public function tarea () {
+        return $this->belongsTo('Tarea','id_tarea');
+    }
+
+    public function proceso () {
+        return $this->belongsTo('Proceso','id_tarea');
+    }
+
+    public function empleado () {
+        return $this->belongsTo('Empleado','id_tarea');
+    }
+
+    public function produccion () {
+        return $this->belongsTo('Produccion','id_tarea');
+    }
+}
+?>
