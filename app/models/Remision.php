c301b8fa03e90c60a3fc16140d9c04f9345cde7f
cesar <kyele936@gmail.com>
2015-01-12 22:11:41 -0600

    se agrego modulo de Recepcion y Remision

---
+++ app/models/Remision.php
@@ -0,0 +1,48 @@
+<?php
+
+class Remision extends Eloquent {
+    use Utiles;
+    
+    protected $table = 'remision';
+
+    protected $primaryKey           = 'id_remision';
+    public    $timestamps           = false;
+    protected $fillable             = array(
+                                               'folio',
+                                               'total',
+                                               'iva',
+                                               'estatus',
+                                               'id_movimiennto_almacen',
+                                               'id_cliente',
+                                               'id_empleado',
+                                               'fecha_registro',              
+                                           );
+    protected static $relaciones    = array(
+                                                'movimiennto_almacen',
+                                                'cliente',
+                                                'empleado',
+                                                'cliente_abono',
+                                                'producto',
+                                            );
+
+    public function movimiennto_almacen () {
+        return $this->belongsTo('MovimientoAlmacen','id_movimiennto_almacen');
+    }
+
+    public function cliente () {
+        return $this->belongsTo('Cliente','id_cliente');
+    }
+
+    public function empleado () {
+        return $this->belongsTo('Empleado','id_empleado');
+    }
+    
+    public function cliente_abono () {
+        return $this->belongsToMany('ClienteAbono','abono_rel_remision','id_remision','id_cliente_abono')->withPivot('monto');     
+    }
+
+    public function producto () {
+        return $this->belongsToMany('Producto','remision_detalle','id_remision','id_producto')->withPivot('cantidad', 'precio_unitario', 'descuento');     
+    }
+}
+?>  
