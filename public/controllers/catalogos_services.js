b9da99cd842f0a0a9337098aedb0bdafbe07dbf5
cesar <kyele936@gmail.com>
2015-02-07 19:32:27 -0600

    Agrege varias rutas appi con  y

--- public/controllers/catalogos_services.js
+++ public/controllers/catalogos_services.js
@@ -4,17 +4,33 @@
     *   @since      02/06/2015
     *   @version    1
     *   @access     public
-    *   @param      callbacks
-    *   @return     promise
+    *   @param      $http , $location , $q 
+    *   @return     
     *   @example    catalogosServices.getDep(function(data){.....}
 */
-angular.module( 'appZapateria' ).service( 'catalogosServices' , [ '$http' , '$location' , '$q' , function ( $http , $location , $q ) {
+angular.module( 'appZapateria' ).service( 'catalogosServices' , [ '$location' , '$q' , '$http' , function ( $location , $q , $http ) {
 
     var path_angular , path_server;
     path_angular = $location.absUrl();
     path_server  = path_angular.substring( 0 , path_angular.indexOf( 'index.html' ) != -1 ? path_angular.indexOf( 'index.html' ):path_angular.indexOf( '#' ) );
     path_server += 'catalogos/';
-    
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getCar(function(data){.....}
+    */    
+    this.getCar = function ( success , failure ) {        
+        $http.get( path_server + 'cargo' )
+            .success(success)
+            .error(failure);
+    }
+
     /**
     *   this function returns the promise that contains a json
     *   @author     Cesar Herrera <kyele936@gmail.com>
@@ -29,7 +45,56 @@ angular.module( 'appZapateria' ).service( 'catalogosServices' , [ '$http' , '$lo
         $http.get( path_server + 'departamento' )
             .success(success)
             .error(failure);
-    } 
+    }
+    //return $resource( path_server + 'departamento');
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getAdj(function(data){.....}
+    */
+    this.getAdj = function ( success , failure ) {        
+        $http.get( path_server + 'adjunto_tipo' )
+            .success(success)
+            .error(failure);
+    }
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getAju_ent(function(data){.....}
+    */
+    this.getAju_ent = function ( success , failure ) {        
+        $http.get( path_server + 'ajuste_entrada_tipo' )
+            .success(success)
+            .error(failure);
+    }
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getAju_sal(function(data){.....}
+    */
+    this.getAju_sal = function ( success , failure ) {        
+        $http.get( path_server + 'ajuste_salida_tipo' )
+            .success(success)
+            .error(failure);
+    }
 
     /**
     *   this function returns the promise that contains a json
@@ -55,6 +120,55 @@ angular.module( 'appZapateria' ).service( 'catalogosServices' , [ '$http' , '$lo
     *   @access     public
     *   @param      callbacks
     *   @return     promise
+    *   @example    catalogosServices.getCli_tipo(function(data){.....}
+    */
+    this.getCli_tipo = function ( success , failure ) {        
+        $http.get( path_server + 'cliente_tipo' )
+            .success(success)
+            .error(failure);
+    }
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getCli_cat(function(data){.....}
+    */
+    this.getCli_cat = function ( success , failure ) {        
+        $http.get( path_server + 'cliente_categoria' )
+            .success(success)
+            .error(failure);
+    }
+
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
+    *   @example    catalogosServices.getCli_cat(function(data){.....}
+    */
+    this.getCli_cat = function ( success , failure ) {        
+        $http.get( path_server + 'cliente_categoria' )
+            .success(success)
+            .error(failure);
+    }
+
+    //----------------Catalogos Dependientes----------------------
+    /**
+    *   this function returns the promise that contains a json
+    *   @author     Cesar Herrera <kyele936@gmail.com>
+    *   @since      02/06/2015
+    *   @version    1
+    *   @access     public
+    *   @param      callbacks
+    *   @return     promise
     *   @example    catalogosServices.getEst(function(data){.....}
     */
     this.getEst = function ( success , failure ) {        
@@ -71,12 +185,23 @@ angular.module( 'appZapateria' ).service( 'catalogosServices' , [ '$http' , '$lo
     *   @access     public
     *   @param      callbacks
     *   @return     promise
-    *   @example    catalogosServices.getEst(function(data){.....}
+    *   @example    catalogosServices.getMun(function(data){.....}
     */
-    this.getEst = function ( success , failure ) {        
-        $http.get( path_server + 'dependientes/estado/1' )
+    this.getMun = function ( success , failure ) {        
+        $http.get( path_server + 'dependientes/municipio/14' )
             .success(success)
             .error(failure);
     }
      
 }]);
+
+angular.module( 'appZapateria' ).service( 'Departamentos' , [ '$resource' , '$location' , 'catalogosServices' , function( $resource , $location , catalogosServices ) {
+    var path_angular , path_server;
+    path_angular = $location.absUrl();
+    path_server  = path_angular.substring( 0 , path_angular.indexOf( 'index.html' ) != -1 ? path_angular.indexOf( 'index.html' ):path_angular.indexOf( '#' ) );
+    path_server += 'catalogos/';
+
+    return $resource ( path_server + 'departamento' , null , {
+        'get': { method:'GET' }
+    });
+}])
--- public/controllers/empleado_ctrl.js
+++ public/controllers/empleado_ctrl.js
@@ -1,14 +1,32 @@
-angular.module('appZapateria').controller('empleadoCtrl', ['$location','$modal','catalogosServices','routeServices','$http', function( $location, $modal, catalogosServices, routeServices, $http) {
+angular.module( 'appZapateria' ).controller( 'empleadoCtrl' , [ '$location' , '$modal' , 'catalogosServices' , 'routeServices' , 'Departamentos' , function( $location , $modal , catalogosServices , routeServices , Departamentos ) {
+
     var empleado = this;    
+
     //Aqui llamo a mi funcion que esta en el service para traer los datos
     catalogosServices.getDep(function(data){
         empleado.departamentos = data;
     });
-
-    catalogosServices.getAlm(function(data){
-        empleado.almacenes = data;
+    /*empleado.departamentos = catalogosServices.get();
+    
+    catalogosServices.getCar(function(data){
+        empleado.cargos = data;
+    });*/
+    catalogosServices.getCar(function(data){
+        empleado.cargos = data;
+        empleado.departamentos = Departamentos.get();
     });
+    
     catalogosServices.getEst(function(data){
         empleado.estados = data;
     });
+
+    catalogosServices.getMun(function(data){
+        empleado.municipios = data;
+    });
+    
+    
+    catalogosServices.getCli_cat(function(data){
+        empleado.categoria_clientes = data;
+    });    
+    
 }]);
